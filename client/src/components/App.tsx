1f2e9e6f4c156acacdd24fc659ea2fe0a7256e6e
shevtsov-dmitry <shadowstakeus28@gmail.com>
2025-06-29 17:29:09 +0300

    init: server with kotlin and gradle without frameworks

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -3,7 +3,7 @@ import "../App.css";
 function App() {
   return (
     <main className="container">
-      <h1 className="text-white bg-red-500 px-10 font-bold py-10">App</h1>
+      <h1 className="bg-red-500 px-10 py-10 font-bold text-white">App</h1>
     </main>
   );
 }
