48090861b8f4a5370cffa126ebe6d9659bec83a1
shevtsov-dmitry <shadowstakeus28@gmail.com>
2025-06-29 17:28:43 +0300

    init: client app with tauri + react + tailwind

---
+++ client/src/components/App.tsx
@@ -0,0 +1,11 @@
+import "../App.css";
+
+function App() {
+  return (
+    <main className="container">
+      <h1 className="text-white bg-red-500 px-10 font-bold py-10">App</h1>
+    </main>
+  );
+}
+
+export default App;
