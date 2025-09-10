2839742eae409723938081cc3dbc4c1619db4540
shevtsov-dmitry <shadowstakeus28@gmail.com>
2025-09-10 15:01:05 +0300

    feat: create server backend of spring boot

--- server/settings.gradle.kts
+++ server/settings.gradle.kts
@@ -1,13 +1,1 @@
-pluginManagement {
-    val quarkusPluginVersion: String by settings
-    val quarkusPluginId: String by settings
-    repositories {
-        mavenCentral()
-        gradlePluginPortal()
-        mavenLocal()
-    }
-    plugins {
-        id(quarkusPluginId) version quarkusPluginVersion
-    }
-}
-rootProject.name="app"
+rootProject.name = "cloudnotes"
---
+++ server/src/main/java/app/cloudnotes/CloudnotesApplication.java
@@ -0,0 +1,13 @@
+package app.cloudnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudnotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudnotesApplication.class, args);
+	}
+
+}
