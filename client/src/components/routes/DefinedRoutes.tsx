5c7e6122f36376d41977548f8ac45ed5733850c3
shevtsov-dmitry <shadowstakeus28@gmail.com>
2025-09-30 21:10:51 +0300

    feat: create component manager of all project roots

---
+++ client/src/components/routes/DefinedRoutes.tsx
@@ -0,0 +1,10 @@
+import { ReactElement } from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+const DefinedRoutes = (): ReactElement => (
+    <Routes>
+        <Route />
+    </Routes>
+)
+
+export default DefinedRoutes
