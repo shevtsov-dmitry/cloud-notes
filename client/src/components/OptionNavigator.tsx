56b72a33914dbc406fb4f0e430a9c3596327bd03
shevtsov-dmitry <shadowstakeus28@gmail.com>
2025-09-30 21:09:56 +0300

    feat(style): add button icon to navigate to settings

--- client/src/components/OptionNavigator.tsx
+++ client/src/components/OptionNavigator.tsx
@@ -5,23 +5,32 @@ const OptionNavigator = (): ReactElement => {
     const navigate = useNavigate()
 
     const styles = {
-        icon: 'w-32 hover:scale-105 hover:cursor-pointer transition select-none',
+        icon: 'hover:scale-105 hover:cursor-pointer transition select-none',
     }
 
     return (
         <section className="bottom-0 flex h-screen w-full items-center justify-center">
-            <div className="flex h-fit w-full flex-row justify-evenly">
-                <button>
+            <div className="absolute top-[1%] right-[2%]">
+                <button onClick={() => navigate('/settings')}>
                     <img
-                        className={styles.icon}
-                        src="images/home/search-icon.svg"
+                        className={`${styles.icon} w-16`}
+                        src="images/home/icons/settings.svg"
                         alt="search"
                     />
                 </button>
-                <button>
+            </div>
+            <div className="flex h-fit w-full flex-row justify-evenly">
+                <button onClick={() => navigate('/search')}>
                     <img
-                        className={styles.icon}
-                        src="images/home/add-icon.svg"
+                        className={`${styles.icon} w-32`}
+                        src="images/home/icons/search.svg"
+                        alt="search"
+                    />
+                </button>
+                <button onClick={() => navigate('/add')}>
+                    <img
+                        className={`${styles.icon} w-32`}
+                        src="images/home/icons/add.svg"
                         alt="add"
                     />
                 </button>
